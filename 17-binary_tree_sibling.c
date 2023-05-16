56abb7d8d28ffe5250b9cd1aed481dda78dc9973
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-16 02:28:16 -0700

    whrite a function that finc the sibling of a node

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - cherche le frère d'un nœud dans un arbre binaire
+ * @node: Pointeur vers le nœud dont on recherche le frère
+ *
+ * Return: Pointeur vers le frère du nœud, ou NULL s'il n'a pas de frère
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	binary_tree_t *parent = node->parent;
+
+	if (parent->left == node)
+		return (parent->right);
+
+	else if (parent->right == node)
+		return (parent->left);
+
+	else
+		return (NULL);
+}
