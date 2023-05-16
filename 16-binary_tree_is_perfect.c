eafe0a0bb00181756be061df0f5446db1a3e447d
Teheremiti <t.tuiaiho@gmail.com>
2023-05-16 03:02:46 -0700

    Fix base case

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,7 +11,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (!tree->left || !tree->right)
+	if (!tree->left && !tree->right)
 		return (1);
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) &&
